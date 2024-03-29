ed5a6af1c0513850455a7e4bd09fe3d776146df1
hasansahin11 <qa.hasansahin@gmail.com>
2024-03-29 00:41:53 -0500

    Update

--- src/test/java/Runners/_04_RunnerForGroups.java
+++ src/test/java/Runners/_04_RunnerForGroups.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@SmokeTest", // Runs the scenarios that has this tag
         features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"},
-        plugin = {"pretty", "html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"} //This is added in the Jenkins build configuration.
+        glue = {"StepDefinitions"}
+//        plugin = {"pretty", "html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"} //This is added in the Jenkins build configuration.
 )
 
 public class _04_RunnerForGroups extends AbstractTestNGCucumberTests {
