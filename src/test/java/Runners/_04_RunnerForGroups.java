fabb68c71e8a3b529d34f5952c281c1bf53eb495
hasansahin11 <qa.hasansahin@gmail.com>
2024-03-28 19:01:13 -0500

    _04_ Runner Plugin has beed added for extent report

--- src/test/java/Runners/_04_RunnerForGroups.java
+++ src/test/java/Runners/_04_RunnerForGroups.java
@@ -4,9 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "@SmokeTest", // Runs the scenarios that that has this tag
+        tags = "@SmokeTest", // Runs the scenarios that has this tag
         features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin = {"pretty", "html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
 )
 
 public class _04_RunnerForGroups extends AbstractTestNGCucumberTests {
