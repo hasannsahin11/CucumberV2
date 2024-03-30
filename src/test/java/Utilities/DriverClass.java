674c5c6e9d2683f40c25b7c5f6b34e541e0079bf
hasansahin11 <qa.hasansahin@gmail.com>
2024-03-30 13:59:56 -0500

    Updated plugins

--- src/test/java/Utilities/DriverClass.java
+++ src/test/java/Utilities/DriverClass.java
@@ -7,52 +7,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-public class DriverClass {
-    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
-    private static ThreadLocal<String> threadDriverName = new ThreadLocal<>();
-
-    public static WebDriver getDriver() {
-        if (threadDriver.get() == null) {
-
-            if (threadDriverName.get() == null) {
-                threadDriverName.set("chrome");
-            }
-
-            switch (threadDriverName.get()) {
-                case "firefox":
-                    threadDriver.set(new FirefoxDriver());
-                    break;
-                case "safari":
-                    threadDriver.set(new SafariDriver());
-                    break;
-                case "edge":
-                    threadDriver.set(new EdgeDriver());
-                    break;
-                default:
-                    threadDriver.set(new ChromeDriver());
-            }
-        }
-        return threadDriver.get();
-    }
-
-
-    public static void quitDriver() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        if (threadDriver.get() != null)
-            threadDriver.get().quit();
-        WebDriver driver = null;
-        threadDriver.set(driver);
-    }
-
-    public static void setThreadDriverName(String browserName) {
-        threadDriverName.set(browserName);
-    }
-}
-
 //public class DriverClass {
 //    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
 //    private static ThreadLocal<String> threadDriverName = new ThreadLocal<>();
@@ -75,20 +29,12 @@ public class DriverClass {
 //                    threadDriver.set(new EdgeDriver());
 //                    break;
 //                default:
-//                    ChromeOptions options = new ChromeOptions();
-//                    if (!runningFromIntelliJ()) {
-//                        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
-//                    }
-//                    threadDriver.set(new ChromeDriver(options));
+//                    threadDriver.set(new ChromeDriver());
 //            }
 //        }
 //        return threadDriver.get();
 //    }
 //
-//    public static boolean runningFromIntelliJ(){
-//        String classPath = System.getProperty("java.class.path");
-//        return classPath.contains("idea_rt.jar");
-//    }
 //
 //    public static void quitDriver() {
 //        try {
@@ -106,5 +52,59 @@ public class DriverClass {
 //        threadDriverName.set(browserName);
 //    }
 //}
+
+public class DriverClass {
+    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+    private static ThreadLocal<String> threadDriverName = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (threadDriver.get() == null) {
+
+            if (threadDriverName.get() == null) {
+                threadDriverName.set("chrome");
+            }
+
+            switch (threadDriverName.get()) {
+                case "firefox":
+                    threadDriver.set(new FirefoxDriver());
+                    break;
+                case "safari":
+                    threadDriver.set(new SafariDriver());
+                    break;
+                case "edge":
+                    threadDriver.set(new EdgeDriver());
+                    break;
+                default:
+                    ChromeOptions options = new ChromeOptions();
+                    if (!runningFromIntelliJ()) {
+                        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
+                    }
+                    threadDriver.set(new ChromeDriver(options));
+            }
+        }
+        return threadDriver.get();
+    }
+
+    public static boolean runningFromIntelliJ(){
+        String classPath = System.getProperty("java.class.path");
+        return classPath.contains("idea_rt.jar");
+    }
+
+    public static void quitDriver() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (threadDriver.get() != null)
+            threadDriver.get().quit();
+        WebDriver driver = null;
+        threadDriver.set(driver);
+    }
+
+    public static void setThreadDriverName(String browserName) {
+        threadDriverName.set(browserName);
+    }
+}
 
 
